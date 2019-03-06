075ed81d0d55870efa3a69b4c6f28ead458bb99a
aur-ml <andrey.teterin@auriga.com>
2019-03-06 10:53:30 +0300

    Linear: check gradients

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -21,6 +21,7 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     
     orig_x = x.copy()
     fx, analytic_grad = f(x)
+    
     assert np.all(np.isclose(orig_x, x, tol)), "Functions shouldn't modify input variables"
 
     assert analytic_grad.shape == x.shape
@@ -31,10 +32,19 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
+        
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
 
-        # TODO compute value of numeric gradient of f to idx
+        # DONE compute value of numeric gradient of f to idx
+        x1 = orig_x.copy()
+        x1[ix] -= delta
+        x2 = orig_x.copy()
+        x2[ix] += delta
+        
+        fx1 = f(x1)[0]
+        fx2 = f(x2)[0]
+        numeric_grad_at_ix = (fx2 - fx1) / (2 * delta)
+
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
